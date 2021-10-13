0d48a5f8cf0e2965f424a54b6aa08c566a951034
Ezequiel <ezej44@hotmail.com>
2021-10-13 10:27:16 -0300

    fix for reload extension

--- background.js
+++ background.js
@@ -1,12 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({
-    listToken: [
-      {
-        id: "bitcoin",
-        symbol: "btc",
-        name: "Bitcoin",
-      },
-    ],
-    currency: "usd",
+  chrome.storage.sync.get(["listToken", "currency"], (data) => {
+    chrome.storage.sync.set({
+      listToken: data.listToken || [
+        {
+          id: "bitcoin",
+          symbol: "btc",
+          name: "Bitcoin",
+        },
+      ],
+      currency: data.currency || "usd",
+    });
   });
 });
